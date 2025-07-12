0e92275156ec3a576cbcaf01f79a563906c3f8d4
Matheus <matheuz6547@gmail.com>
2025-07-12 20:14:58 -0300

    Update cartasupertrunfo.c

--- cartasupertrunfo.c
+++ cartasupertrunfo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#cludine <stdio.h>
 
 int main(){
 
@@ -11,6 +11,7 @@ int populacao1;
 float area1;
 float pib1;
 int pontos1;
+float densidade1, pibPerCapita1;
 
 //Variaveis da carta 2
 
@@ -21,6 +22,7 @@ int populacao2;
 float area2;
 float pib2;
 int pontos2;
+float densidade2, pibPerCapita2;
 
 //Entrada dos dados da carta 1
 
@@ -46,6 +48,10 @@ scanf("%d",  &populacao1);
 
  printf("Digite o numero de pontos turisticos:");
  scanf("%d",  &pontos1);
+
+  // ===== Cálculo - Carta 1 =====
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000) / populacao1;
 
 
 //Entrada dos dados da carta 2
@@ -73,13 +79,36 @@ scanf("%d",  &populacao2);
  printf("Digite o numero de pontos turisticos:");
  scanf("%d",  &pontos2);
 
+ 
 
+    // ===== Cálculo - Carta 2 =====
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000) / populacao2;
 
+    // ===== Exibição dos Resultados =====
+    printf("\n===== RESULTADOS =====\n");
 
+    printf("\n--- Carta 1 ---\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Pontos turisticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km\n", densidade1);
+    printf("PIB per Capita: R$ %.2f\n", pibPerCapita1);
 
+    printf("\n--- Carta 2 ---\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Pontos turisticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km\n", densidade2);
+    printf("PIB per Capita: R$ %.2f\n", pibPerCapita2);
 
-
-
-
-
+    return 0;
 }
